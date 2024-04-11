0ff0e08b1c00cdd102da46f6ca2a0193a762f2ff
tarunmalik330 <maliktarun2005@gmail.com>
2024-04-11 12:43:45 +0530

    change in card shadow

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,14 +3,14 @@ import { Carddata } from "./MapData";
 
 const Services = () => {
   return (
-    <> 
+    <>
       <div className="bg-[#040403] py-16">
         <div className="container xl:max-w-[1320px] px-3 mx-auto">
           <div className="flex flex-row flex-wrap justify-center gap-6">
             {Carddata.map((data, index) => {
               return (
                 <div className={`${data.width} flex`} key={index}>
-                  <div className="bg_card rounded-[16px] relative hover:bg-gradientbg group hover:border-transparent transition-colors ease-linear duration-500 inline-block cursor-pointer py-[40px] px-[32px] ">
+                  <div className="bg-[#0E0D0D] !shadow-[0px,_0px,_0px,_1px_#F4F4F51A] rounded-[16px] relative hover:bg-gradientbg group hover:border-transparent transition-colors ease-linear duration-500 inline-block cursor-pointer py-[40px] px-[32px] ">
                     <div className="pb-4 relative z-10">{data.svg}</div>
                     <h3 className="text-[#E4E4E7] text-xs font-semibold leading-[120%] pb-4 relative z-10">
                       {data.heading}
